9eb63ceb1cac667837bb112aaa39d79a1b3d2db4
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-10-08 15:24:37 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Profile } from 'components/Profil/Profil';
 import './index.css';
+import user from 'components/Profil/user.json';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   
     <App />
+    <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+     
   </React.StrictMode>
 );
+
