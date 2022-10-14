acf3579c97d1fe0b86086f9a2fef247a63d7f59c
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-10-14 19:32:43 +0200

    .

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,12 +1,14 @@
-
+import styles from "./FriendList.module.css"
+  
 export const FriendList = ({ friends }) => (
-  <ul className="friend-list">
+  <ul className={styles.friendList}>
     {friends.map(({ id, name, avatar, isOnline }) => (
-      <li key={id} className="item">
-        <span className="status">{isOnline}</span>
-        <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <li key={id} className={styles.item}>
+        <span className={isOnline ? styles.active : styles.inActive} ></span>
+        <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
         <p className="name">{name}</p>
       </li>
     ))}
   </ul>
+  
 );
