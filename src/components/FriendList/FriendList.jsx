6532c5372e0d1f5c25c233d937d0931834434a10
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-10-14 15:53:04 +0200

    .

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,33 +1,12 @@
 
-
-export const FriendList = ({ friends,children}) => {
-    
-    const render = friends.map((friend) => {
-        return (
-            <>
-                    <FriendListItem
-                        avatar={friend.avatar}
-                        name={friend.name}
-                        id={friend.id}
-                        isOnline={friend.isOnline } />
-            </>
-        );
-    });
-
-    return (
-
-            <ul className="friend-list">
-            {render}
-            </ul>);
-};
-
-
-export  const FriendListItem = ({friends,avatar,name,isOnline,id}) => {
-return(
-<li key={id} className="item">
-    <span className="status">{isOnline }</span>
-  <img className="avatar" src={avatar} alt="User avatar" width="48" />
-        <p className="name">{name }</p>
-</li>
-)
-}
+export const FriendList = ({ friends }) => (
+  <ul className="friend-list">
+    {friends.map(({ id, name, avatar, isOnline }) => (
+      <li key={id} className="item">
+        <span className="status">{isOnline}</span>
+        <img className="avatar" src={avatar} alt="User avatar" width="48" />
+        <p className="name">{name}</p>
+      </li>
+    ))}
+  </ul>
+);
