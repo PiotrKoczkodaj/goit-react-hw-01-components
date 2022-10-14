5fd7f6e421b57bb7497146586870cbd7f1aae13b
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-10-14 15:00:08 +0200

    .

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { App } from 'components/App';
 import { Profile } from 'components/Profil/Profil';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
-import { FriendListItem } from 'components/FriendList/FriendList';
 import './index.css';
 import user from 'components/Profil/user.json';
 import data from 'components/Statistics/data.json';
