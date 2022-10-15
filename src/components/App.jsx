4815ecc9dac6a57d50d3fe55788d74049aa7702f
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-10-15 15:43:40 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,31 @@
 
+import { Profile } from 'components/Profil/Profil';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+
+import user from 'components/Profil/user.json';
+import data from 'components/Statistics/data.json';
+import friend from 'components/FriendList/friends.json';
+import transactions from 'components/TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-        padding: '50px 10px',
-      }}
-    >
-    
-    </div>
+    <>
+         <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+    />
+    <Statistics
+      title="Upload stats"
+      stats = {data}
+    />
+    <FriendList friends={friend} />
+    <TransactionHistory items={transactions}/>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { App } from 'components/App';
-import { Profile } from 'components/Profil/Profil';
-import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-
-import user from 'components/Profil/user.json';
-import data from 'components/Statistics/data.json';
-import friend from 'components/FriendList/friends.json';
-import transactions from 'components/TransactionHistory/transactions.json';
-
+import { App } from './components/App';
 
 import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-     <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-    />
-    <Statistics
-      title="Upload stats"
-      stats = {data}
-    />
-    <FriendList friends={friend} />
-    <TransactionHistory items={transactions}/>
     </React.StrictMode>
 );
 
